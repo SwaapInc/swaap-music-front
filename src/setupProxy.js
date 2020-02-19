3cfc62636a20a0254e56255161dc7dd428c02c15
Bedjai Julien <julien.bedjai@efrei.net>
2020-02-19 09:14:48 +0100

    test proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(proxy(
         '/api', {
-            target: 'http://localhost:1234',
+            target: 'https://swaap-music-back.herokuapp.com',
             changeOrigin: true,
         }
     ));
